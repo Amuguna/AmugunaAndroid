340ebb157a01c526e9e1cadcb0d2b7b3dcf62d9f
ranokim <daesub0515@gmail.com>
2016-07-27 23:36:23 +0900

    Random Selection has been added.

--- app/src/main/java/com/nexters/amuguna/gola/ResultActivity.java
+++ app/src/main/java/com/nexters/amuguna/gola/ResultActivity.java
@@ -8,8 +8,12 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.Collections;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import info.hoang8f.widget.FButton;
 
 /**
  * Created by Daesub Kim on 2016-07-15.
@@ -20,6 +24,12 @@ public class ResultActivity extends AppCompatActivity {
 
     @Bind(com.nexters.amuguna.gola.R.id.result_img)
     ImageView resultImg;
+
+    @Bind(com.nexters.amuguna.gola.R.id.retryBtn)
+    FButton retryBtn;
+
+    @Bind(com.nexters.amuguna.gola.R.id.goHomeBtn)
+    FButton goHomeBtn;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +47,38 @@ public class ResultActivity extends AppCompatActivity {
             Log.e("isTournament ?", "TOURNAMENT !!!! ");
             Log.e("RESULT", ""+intent.getIntExtra("result",10));
         } else {
+
+            /* 랜덤 수를 한번 섞어준다 */
+            Collections.shuffle(StaticInfo.RAN);
+
             Log.e("isTournament ?", "NO !!!! ");
+            Log.e("RESULT", ""+StaticInfo.RAN.get(0));
         }
     }
+
+    @OnClick(com.nexters.amuguna.gola.R.id.retryBtn)
+    void retryBtnClick() {
+
+        if(intent.getBooleanExtra("isTournament", true)){
+            /* Move to TournamentActivity. */
+            Intent intent = new Intent(ResultActivity.this,TournamentActivity.class);
+            intent.putExtra("isTournament", true);
+            intent.putExtra("isFirstRound", true);
+            intent.putExtra("round", StaticInfo.DEFAULT_ROUND);
+            startActivity(intent);
+        } else {
+            /* Move to ResultActivity. */
+            Intent intent = new Intent(ResultActivity.this,ResultActivity.class);
+            intent.putExtra("isTournament", false);
+            startActivity(intent);
+        }
+
+    }
+
+    @OnClick(com.nexters.amuguna.gola.R.id.goHomeBtn)
+    void goHomeBtnClick() {
+        Intent intent = new Intent(ResultActivity.this,GolaMainActivity.class);
+        startActivity(intent);
+    }
+
 }
